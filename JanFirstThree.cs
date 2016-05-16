28e3b16f7087230ba150c065932f97e74ac597a7
Your Name <you.example.com>
2016-05-16 18:53:47 -0400

    submitting ConsoleApplication8

---
+++ JanFirstThree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Any Integer: ");
+            int anyInteger = int.Parse(Console.ReadLine());
+            if (anyInteger < 10)
+            {
+                Console.WriteLine("This number is too small");
+            }
+            else
+            {
+                Console.WriteLine("This number is big enough");
+            }
+        }
+    }
+}
